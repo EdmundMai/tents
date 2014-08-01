50b89ee36892c166259ad93abd068e11b965c7df
Edmund <edmundmai@gmail.com>
2014-08-01 18:59:41 -0400

    fixed some tests

--- features/step_definitions/public/login_steps.rb
+++ features/step_definitions/public/login_steps.rb
@@ -1,10 +1,13 @@
 Given(/^I am an authenticated customer$/) do
   user = FactoryGirl.create(:user)
   visit new_user_session_path
-  fill_in("user[email]", with: user.email)
-  fill_in("user[password]", with: "test123")
-  check("user[remember_me]")
-  click_button("Sign in")
+  within(".login_div") do
+    fill_in("user[email]", with: user.email)
+    fill_in("user[password]", with: "test123")
+    check("user[remember_me]")
+    click_button("Sign in")
+  end
+
 end
 
 Then(/^I should be redirected back to the homepage$/) do
